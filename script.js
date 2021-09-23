1da7f0da414c53e19527b396369a8729ea312456
Ivan MPR <ivsanmas@gmail.com>
2021-09-23 20:37:18 +0200

    dev

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ const userError = document.querySelector('.username-error');
 const emailInput = document.getElementById('email-input');
 const emailError = document.querySelector('.email-error');
 
+const passwordInput = document.getElementById('password-input');
+const passwordError = document.querySelector('.password-error');
+
+const password2Input = document.getElementById('password2-input');
+const password2Error = document.querySelector('.password2-error');
+
 window.addEventListener('load', function () {
   errorParagraphs.forEach(p => p.classList.add('hidden'));
 });
@@ -17,6 +23,11 @@ btn.addEventListener('click', e => {
   e.preventDefault();
   checkUsernameInput(userInput.value);
   checkEmailInput(emailInput.value);
+  checkPasswordInput(passwordInput.value);
+  checkSecondPassword(
+    checkPasswordInput(passwordInput.value),
+    password2Input.value
+  );
 });
 
 function checkUsernameInput(str) {
@@ -31,6 +42,22 @@ function checkEmailInput(str) {
     show(emailError, emailInput);
   } else {
     hide(emailInput, emailError);
+  }
+}
+function checkPasswordInput(str) {
+  if (str.length >= 6) {
+    show(passwordError, passwordInput);
+  } else {
+    hide(passwordInput, passwordError);
+  }
+  return str;
+}
+
+function checkSecondPassword(firstPass, secondPass) {
+  if (firstPass === secondPass && secondPass !== '' && secondPass.length >= 6) {
+    show(password2Error, password2Input);
+  } else {
+    hide(password2Input, password2Error);
   }
 }
 function show(errEl, el) {
