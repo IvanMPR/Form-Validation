97fefc2df45a5ad44d5cc9d077a271e16b0135b9
Ivan MPR <ivsanmas@gmail.com>
2021-09-24 11:09:16 +0200

    isSubmitSuccesfull () added

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const btn = document.querySelector('button');
 const resetBtn = document.querySelector('.reset-btn');
 const errorParagraphs = document.querySelectorAll('.error');
+const successMsg = document.querySelector('.success');
 
 const userInput = document.getElementById('user-input');
 const userError = document.querySelector('.username-error');
@@ -18,10 +19,14 @@ const password2Error = document.querySelector('.password2-error');
 
 window.addEventListener('load', function () {
   errorParagraphs.forEach(p => p.classList.add('hidden'));
+  successMsg.classList.remove('opaque');
+  successMsg.classList.add('transparent');
 });
 resetBtn.addEventListener('click', () => {
   document.querySelectorAll('input').forEach(input => {
     input.classList.remove('green', 'red-border');
+    successMsg.classList.remove('opaque');
+    successMsg.classList.add('transparent');
   });
   errorParagraphs.forEach(para => {
     para.classList.remove('visible');
@@ -37,6 +42,7 @@ btn.addEventListener('click', e => {
     checkPasswordInput(passwordInput.value),
     password2Input.value
   );
+  isSubmitSuccesfull();
 });
 
 function checkUsernameInput(str) {
@@ -67,6 +73,13 @@ function checkSecondPassword(firstPass, secondPass) {
     show(password2Error, password2Input);
   } else {
     hide(password2Input, password2Error);
+  }
+}
+function isSubmitSuccesfull() {
+  const inputs = Array.from(document.querySelectorAll('input'));
+  if (inputs.every(input => input.classList.contains('green'))) {
+    successMsg.classList.remove('transparent');
+    successMsg.classList.add('opaque');
   }
 }
 function show(errEl, el) {
